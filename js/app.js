aa96518cb79391721febf7761dfab285a453e7df
ViniciusFreiry <viniciusfreiry@gmail.com>
2025-05-19 22:17:16 -0300

    Implementing List Shuffling Function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 const listaAmigos = document.getElementById("lista-amigos");
+let amigos = [];
 
 function adicionar() {
+    // Adicionando Pessoa na Lista
     const amigo = document.getElementById("nome-amigo");
+    amigos.push(amigo.value);
 
     if (listaAmigos.innerHTML == "") {
         listaAmigos.innerHTML = amigo.value;
@@ -13,7 +16,16 @@ function adicionar() {
 }
 
 function sortear() {
+    // Embaralhando a Lista de Amigos
+    embaralhaLista(amigos);
+}
 
+function embaralhaLista(lista) {
+    for (let i = lista.length; i > 0; i--) {
+        const indiceAleatorio = Math.floor(Math.random() * i);
+
+        [lista[i - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[i - 1]];
+    }
 }
 
 function reiniciar() {
